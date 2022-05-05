0945db6bd5759f4811595c8e2e1249c498848182
Abhikhush28 <103246309+Abhikhush28@users.noreply.github.com>
2022-05-05 09:30:35 +0530

    Create subtract_without_using_minus_Operator.c

---
+++ subtract_without_using_minus_Operator.c
@@ -0,0 +1,16 @@
+// Subtract two numbers without using subtarction operator
+
+#include<stdio.h>
+int main()
+{
+  int a, b, sum;
+
+  printf("Enter any two integers: ");
+  scanf("%d%d",&a,&b);
+
+  sum = a + ~b + 1;
+
+  printf("Difference of two integers: %d",sum);
+
+  return 0;
+}
